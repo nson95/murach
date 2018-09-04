6ab7dc92d806aa329b14bf5fc1c470ce2b3d0abb
nson95 <nicksonnenberg95@gmail.com>
2018-09-03 21:38:05 -0400

    more practice, hard at work.

--- ch01_test/src/TestApp.java
+++ ch01_test/src/TestApp.java
@@ -1,5 +1,7 @@
 public class TestApp {
     public static void main(String[] args)  {
         System.out.println("Success!!");
+        System.out.println("Success!!");
+        System.out.println("Failure..just kidding, this is obviously a success.");
     }
 }
